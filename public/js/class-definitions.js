77bb80d986fb2c1dbddb77250350c218573b25f6
Mika <mikadewitz@gmail.com>
2015-02-04 15:14:33 -1000

    adding House class

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1053,7 +1053,9 @@ function Shoe(size, color){
  * House class
  * @param {number} stories How many stories tall the house is
  */
-
+function House(stories){
+  this.stories = stories;
+}
 
 /**
  * Step 57
